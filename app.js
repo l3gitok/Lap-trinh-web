8513ad962a488e17ae50e88bbf4ad4c9b3a4921f
Tuan <l3gitok@gmail.com>
2024-12-24 23:25:58 +0700

    Update code

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/api", (req, res) => {
+  res.json({ message: "API is working!" });
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
