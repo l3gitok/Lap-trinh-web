f533d1c987181d55c581bf5d40c8969722cef4bb
Tuan <l3gitok@gmail.com>
2024-12-25 09:18:00 +0700

    uu

--- app.js
+++ app.js
@@ -34,8 +34,4 @@ app.use('/api/social-media', socialmediaRoutes);
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ error: err.message });
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 module.exports = app;
