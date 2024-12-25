a91efbd20384b2969b13779521aee1b5f9e29be0
Tuan <l3gitok@gmail.com>
2024-12-25 09:36:39 +0700

    luk

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'https://backendlinktree.onrender.com/api',
+  baseURL: 'https://backend-eux0.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
